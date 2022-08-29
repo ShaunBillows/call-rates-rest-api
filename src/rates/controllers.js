3ee7a4c360253676a316a36ef5eef201334dbc73
ShaunBillows <billows.shaun@gmail.com>
2022-08-29 17:43:38 +0100

    paginated responses functional

--- src/rates/controllers.js
+++ src/rates/controllers.js
@@ -1,22 +1,24 @@
+const e = require("express");
 const Rate = require("./model");
 
 exports.readRates = async (req, res) => {
   console.log("Request recieved readRates.");
   try {
-    if (!req.header("region") && !req.header("prefix")) {
-      throw new Error("No query detected - 'region' or 'prefix' is required in the request header.")   
-    }
+    const page = parseInt(req.query.page);
+    const limit = parseInt(req.query.limit);
+    const skipIndex = (page - 1) * limit;
     let result
-    if (req.header("prefix")) {
-      result = await Rate.find({ Prefix: new RegExp(req.header("prefix")) }) // Substring search, case sensitive.
+    if (req.query.prefix) {
+      result = await Rate.find({ Prefix: new RegExp(req.query.prefix, 's')}).sort({ Prefix: 1 }).limit(limit).skip(skipIndex).exec() // Substring search, starts with...
     } else {
-      result = await Rate.find({ Country: new RegExp(req.header("region", 'i')) }); // Substring search, case insensitive.
+      result = await Rate.find({ Country: new RegExp(req.query.region, 'i') }).sort({ Country: 1 }).limit(limit).skip(skipIndex).exec() // Substring search, case insensitive.
     }
     if (!result || result.length === 0) {
       throw new Error("No documents found.")
+    } else {
+      console.log(result);
+      res.status(200).send({ result });
     }
-    console.log(result);
-    res.status(200).send({ result });
   } catch (error) {
     console.log(error);
     res.status(500).send({ err: error.message });
--- src/server.js
+++ src/server.js
@@ -1,14 +1,12 @@
-require("./db/connection") //Run's database connection immediately
+require("./db/connection") 
 const express = require("express")
-// add routes and controllers
 const userRouter = require("./rates/routes");
 const app = express();
 const port = process.env.PORT || 5001
 
-//add relevant routes and controllers to app before listen runs
-app.use(express.json()); //Tell entire server that it will always recieve JSON, and to always send back JSON
-app.use(userRouter);
+app.use(express.json())
+app.use(userRouter)
 
 app.listen(port, () => {
-    console.log("Listening on port 5001.");
-  });
+    console.log("Listening on port 5001.")
+  })
