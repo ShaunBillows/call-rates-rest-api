268e8ba2764ad232a99a267885f3156b0fdee035
ShaunBillows <billows.shaun@gmail.com>
2022-08-31 14:26:08 +0100

    installed cors

--- src/server.js
+++ src/server.js
@@ -2,9 +2,11 @@ require("./db/connection");
 const express = require("express");
 const userRouter = require("./rates/routes");
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5001;
 
 app.use(express.json());
+app.use(cors());
 app.use(userRouter);
 
 app.listen(port, () => {
